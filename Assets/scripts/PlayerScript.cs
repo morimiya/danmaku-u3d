27a196fd3cb14452f673ab8daf5719f2cc5e5776
morimiya <morimiya@qq.com>
2018-03-04 14:10:29 +0800

    BaseVer

---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	public Vector2 speed = new Vector2 (50, 50);
+	private Vector2 movement;
+	private Rigidbody2D rigidbodyComponent;
+
+	// Use this for initialization
+	//void Start () {
+	//
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+		float inputX = Input.GetAxis ("Horizontal");
+		float inputY = Input.GetAxis ("Vertical");
+
+		movement = new Vector2 (
+			speed.x * inputX, 
+			speed.y * inputY
+		);
+
+		bool shoot = Input.GetButton("Fire1");
+		shoot |= Input.GetButtonDown("Fire2");
+
+		if (shoot) {
+			WeaponScript weapon = GetComponent<WeaponScript>();
+			if (weapon != null) {
+				weapon.Attack(false);
+			}
+		}
+
+		// 6 - Make sure we are not outside the camera bounds
+	    var dist = (transform.position - Camera.main.transform.position).z;
+
+	    var leftBorder = Camera.main.ViewportToWorldPoint(
+	      new Vector3(0, 0, dist)
+	    ).x;
+
+	    var rightBorder = Camera.main.ViewportToWorldPoint(
+	      new Vector3(1, 0, dist)
+	    ).x;
+
+	    var topBorder = Camera.main.ViewportToWorldPoint(
+	      new Vector3(0, 0, dist)
+	    ).y;
+
+	    var bottomBorder = Camera.main.ViewportToWorldPoint(
+	      new Vector3(0, 1, dist)
+	    ).y;
+
+	    transform.position = new Vector3(
+	      Mathf.Clamp(transform.position.x, leftBorder, rightBorder),
+	      Mathf.Clamp(transform.position.y, topBorder, bottomBorder),
+	      transform.position.z
+	    );
+
+	    // End of the update method
+	}
+
+	void FixedUpdate() {
+		if (rigidbodyComponent == null)
+			rigidbodyComponent = GetComponent<Rigidbody2D> ();
+
+		rigidbodyComponent.velocity = movement;
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # danmaku-u3d
+Unity Ver:5.3.4f1
 Just a tutorial.
